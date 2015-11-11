8909746b23d4772eb9b56f89bb293dcb41347b5f
DeutschMark <mirque84@gmail.com>
2015-11-11 13:51:50 +0100

    Bugfix, thanks wei

--- ShowNodeCount.glyphsReporter/Contents/Resources/ShowNodeCount.py
+++ ShowNodeCount.glyphsReporter/Contents/Resources/ShowNodeCount.py
@@ -32,9 +32,6 @@ GlyphsReporterProtocol = objc.protocolNamed( "GlyphsReporter" )
 class ShowNodeCount ( NSObject, GlyphsReporterProtocol ):
 	
 	def init( self ):
-		"""
-		Put any initializations you want to make here.
-		"""
 		try:
 			#Bundle = NSBundle.bundleForClass_( NSClassFromString( self.className() ));
 			return self
@@ -42,70 +39,30 @@ class ShowNodeCount ( NSObject, GlyphsReporterProtocol ):
 			self.logToConsole( "init: %s" % str(e) )
 	
 	def interfaceVersion( self ):
-		"""
-		Distinguishes the API version the plugin was built for. 
-		Return 1.
-		"""
 		try:
 			return 1
 		except Exception as e:
 			self.logToConsole( "interfaceVersion: %s" % str(e) )
 	
 	def title( self ):
-		"""
-		This is the name as it appears in the menu in combination with 'Show'.
-		E.g. 'return "Nodes"' will make the menu item read "Show Nodes".
-		"""
 		try:
 			return "* Node Count"
 		except Exception as e:
 			self.logToConsole( "title: %s" % str(e) )
 	
 	def keyEquivalent( self ):
-		"""
-		The key for the keyboard shortcut. Set modifier keys in modifierMask() further below.
-		Pretty tricky to find a shortcut that is not taken yet, so be careful.
-		If you are not sure, use 'return None'. Users can set their own shortcuts in System Prefs.
-		"""
 		try:
 			return None
 		except Exception as e:
 			self.logToConsole( "keyEquivalent: %s" % str(e) )
 	
 	def modifierMask( self ):
-		"""
-		Use any combination of these to determine the modifier keys for your default shortcut:
-			return NSShiftKeyMask | NSControlKeyMask | NSCommandKeyMask | NSAlternateKeyMask
-		Or:
-			return 0
-		... if you do not want to set a shortcut.
-		"""
 		try:
 			return 0
 		except Exception as e:
 			self.logToConsole( "modifierMask: %s" % str(e) )
 	
 	def drawForegroundForLayer_( self, Layer ):
-		"""
-		Whatever you draw here will be displayed IN FRONT OF the paths.
-		Setting a color:
-			NSColor.colorWithCalibratedRed_green_blue_alpha_( 1.0, 1.0, 1.0, 1.0 ).set() # sets RGBA values between 0.0 and 1.0
-			NSColor.redColor().set() # predefined colors: blackColor, blueColor, brownColor, clearColor, cyanColor, darkGrayColor, grayColor, greenColor, lightGrayColor, magentaColor, orangeColor, purpleColor, redColor, whiteColor, yellowColor
-		Drawing a path:
-			myPath = NSBezierPath.alloc().init()  # initialize a path object myPath
-			myPath.appendBezierPath_( subpath )   # add subpath to myPath
-			myPath.fill()   # fill myPath with the current NSColor
-			myPath.stroke() # stroke myPath with the current NSColor
-		To get an NSBezierPath from a GSPath, use the bezierPath() method:
-			myPath.bezierPath().fill()
-		You can apply that to a full layer at once:
-			if len( myLayer.paths > 0 ):
-				myLayer.bezierPath()       # all closed paths
-				myLayer.openBezierPath()   # all open paths
-		See:
-		https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSBezierPath_Class/Reference/Reference.html
-		https://developer.apple.com/library/mac/documentation/cocoa/reference/applicationkit/classes/NSColor_Class/Reference/Reference.html
-		"""
 		try:
 			pass
 		except Exception as e:
@@ -116,7 +73,7 @@ class ShowNodeCount ( NSObject, GlyphsReporterProtocol ):
 
 		Glyph = Layer.parent
 		Font = Glyph.parent
-		selectedLayers = Font.selectedLayers
+		selectedLayer = Font.selectedLayers[0]
 
 		xHeight = Font.selectedFontMaster.xHeight
 		angle = Font.selectedFontMaster.italicAngle
@@ -124,37 +81,25 @@ class ShowNodeCount ( NSObject, GlyphsReporterProtocol ):
 		offset = math.tan(math.radians(angle)) * xHeight/2
 
 		nodeCounter = 0
-		for thisLayer in selectedLayers:
-			for thisPath in thisLayer.paths:
-				nodeCounter += len(thisPath.nodes)
+		for thisPath in selectedLayer.paths:
+			nodeCounter += len(thisPath.nodes)
 
-			#self.drawTextAtPoint( "%s Nodes" % nodeCounter, (5, 20) )
-			self.drawTextAtPoint( u"· %s" % nodeCounter, (-15 - offset, 5) )
+		self.drawTextAtPoint( u"· %s" % nodeCounter, (-15 - offset, 5) )
 
 
 	def drawBackgroundForLayer_( self, Layer ):
-		"""
-		Whatever you draw here will be displayed BEHIND the paths.
-		"""
 		try:
 			self.drawNodeCount( Layer )
 		except Exception as e:
 			self.logToConsole( "drawBackgroundForLayer_: %s" % str(e) )
 
 	def drawBackgroundForInactiveLayer_( self, Layer ):
-		"""
-		Whatever you draw here will be displayed behind the paths,
-		but for inactive glyphs in the Edit view.
-		"""
 		try:
 			pass
 		except Exception as e:
 			self.logToConsole( "drawBackgroundForInactiveLayer_: %s" % str(e) )
 	
 	def drawTextAtPoint( self, text, textPosition, fontSize=10.0, fontColor=NSColor.colorWithCalibratedRed_green_blue_alpha_( 0.4, 0, .6, 1 ) ):
-		"""
-		Use self.drawTextAtPoint( "blabla", myNSPoint ) to display left-aligned text at myNSPoint.
-		"""
 		try:
 			glyphEditView = self.controller.graphicView()
 			currentZoom = self.getScale()
@@ -168,22 +113,9 @@ class ShowNodeCount ( NSObject, GlyphsReporterProtocol ):
 			self.logToConsole( "drawTextAtPoint: %s" % str(e) )
 	
 	def needsExtraMainOutlineDrawingForInactiveLayer_( self, Layer ):
-		"""
-		Whatever you draw here will be displayed in the Preview at the bottom.
-		Remove the method or return True if you want to leave the Preview untouched.
-		Return True to leave the Preview as it is and draw on top of it.
-		Return False to disable the Preview and draw your own.
-		In that case, don't forget to add Bezier methods like in drawForegroundForLayer_(),
-		otherwise users will get an empty Preview.
-		"""
 		return True
 	
 	def getHandleSize( self ):
-		"""
-		Returns the current handle size as set in user preferences.
-		Use: self.getHandleSize() / self.getScale()
-		to determine the right size for drawing on the canvas.
-		"""
 		try:
 			Selected = NSUserDefaults.standardUserDefaults().integerForKey_( "GSHandleSize" )
 			if Selected == 0:
@@ -197,10 +129,6 @@ class ShowNodeCount ( NSObject, GlyphsReporterProtocol ):
 			return 7.0
 
 	def getScale( self ):
-		"""
-		self.getScale() returns the current scale factor of the Edit View UI.
-		Divide any scalable size by this value in order to keep the same apparent pixel size.
-		"""
 		try:
 			return self.controller.graphicView().scale()
 		except:
@@ -208,18 +136,11 @@ class ShowNodeCount ( NSObject, GlyphsReporterProtocol ):
 			return 1.0
 	
 	def setController_( self, Controller ):
-		"""
-		Use self.controller as object for the current view controller.
-		"""
 		try:
 			self.controller = Controller
 		except Exception as e:
 			self.logToConsole( "Could not set controller" )
 	
 	def logToConsole( self, message ):
-		"""
-		The variable 'message' will be passed to Console.app.
-		Use self.logToConsole( "bla bla" ) for debugging.
-		"""
 		myLog = "Show %s plugin:\n%s" % ( self.title(), message )
 		NSLog( myLog )
