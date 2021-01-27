572e7d561a56b86a839cc07de4d0a0ac724c0eba
Mark Frömberg <mirque84@gmail.com>
2021-01-27 19:42:44 +0100

    Update plugin.py

--- ShowNodeCount.glyphsReporter/Contents/Resources/plugin.py
+++ ShowNodeCount.glyphsReporter/Contents/Resources/plugin.py
@@ -17,7 +17,7 @@ import objc
 from GlyphsApp import *
 from GlyphsApp.plugins import *
 
-import sys, os, re
+# import sys, os, re
 import math
 
 GlyphsReporterProtocol = objc.protocolNamed( "GlyphsReporter" )
@@ -27,36 +27,19 @@ class ShowNodeCount(ReporterPlugin):
 	@objc.python_method
 	def settings(self):
 		self.menuName = Glyphs.localize({
-			'en': 'My Plugin',
-			'de': 'Mein Plugin',
-			'fr': 'Ma extension',
-			'es': 'Mi plugin',
-			'pt': 'Meu plug-in',
+			'en': 'Node Count',
 			})
-		self.generalContextMenus = [{
-			'name': Glyphs.localize({
-				'en': 'Do something',
-				'de': 'Tu etwas',
-				'fr': 'Faire quelque chose',
-				'es': 'Hacer algo',
-				'pt': 'Faça alguma coisa',
-				}), 
-			'action': self.doSomething_
-			}]
 
 	@objc.python_method
-	def drawTextAtPoint( self, text, textPosition, fontSize=10.0, fontColor=NSColor.colorWithCalibratedRed_green_blue_alpha_( 0.4, 0, .6, 1 ) ):
-		try:
-			glyphEditView = self.controller.graphicView()
-			currentZoom = self.getScale()
-			fontAttributes = { 
-				NSFontAttributeName: NSFont.labelFontOfSize_( fontSize/currentZoom ),
-				NSForegroundColorAttributeName: fontColor }
-			displayText = NSAttributedString.alloc().initWithString_attributes_( text, fontAttributes )
-			textAlignment = 2 # top left: 6, top center: 7, top right: 8, center left: 3, center center: 4, center right: 5, bottom left: 0, bottom center: 1, bottom right: 2
-			glyphEditView.drawText_atPoint_alignment_( displayText, textPosition, textAlignment )
-		except Exception as e:
-			self.logToConsole( "drawTextAtPoint: %s" % str(e) )			
+	def drawTextAtPoint( self, text, textPosition, fontSize=12.0, fontColor=NSColor.colorWithCalibratedRed_green_blue_alpha_( 0.4, 0, .6, 1 ) ):
+		glyphEditView = self.controller.graphicView()
+		currentZoom = self.getScale()
+		fontAttributes = { 
+			NSFontAttributeName: NSFont.labelFontOfSize_( fontSize/currentZoom ),
+			NSForegroundColorAttributeName: fontColor }
+		displayText = NSAttributedString.alloc().initWithString_attributes_( text, fontAttributes )
+		textAlignment = 2 # top left: 6, top center: 7, top right: 8, center left: 3, center center: 4, center right: 5, bottom left: 0, bottom center: 1, bottom right: 2
+		displayText.drawAtPoint_( textPosition )
 
 	@objc.python_method
 	def drawNodeCount( self, Layer ):
@@ -73,14 +56,11 @@ class ShowNodeCount(ReporterPlugin):
 		for thisPath in selectedLayer.paths:
 			nodeCounter += len(thisPath.nodes)
 
-		self.drawTextAtPoint( u"· %s" % nodeCounter, (-15 - offset, 5) )
+		self.drawTextAtPoint( u"· %s" % nodeCounter, (-25 - offset, 5) )
 
 	@objc.python_method
 	def background(self, layer):
-		try:
-			self.drawNodeCount( Layer )
-		except Exception as e:
-			self.logToConsole( "drawBackgroundForLayer_: %s" % str(e) )
+		self.drawNodeCount( layer )
 	
 	# @objc.python_method
 	# def inactiveLayer(self, layer):
@@ -110,7 +90,6 @@ class ShowNodeCount(ReporterPlugin):
 		try:
 			return self.controller.graphicView().scale()
 		except:
-			self.logToConsole( "Scale defaulting to 1.0" )
 			return 1.0
 
 	@objc.python_method
@@ -118,9 +97,5 @@ class ShowNodeCount(ReporterPlugin):
 		try:
 			self.controller = Controller
 		except Exception as e:
-			self.logToConsole( "Could not set controller" )
+			pass
 
-	@objc.python_method
-	def logToConsole( self, message ):
-		myLog = "Show %s plugin:\n%s" % ( self.title(), message )
-		NSLog( myLog )
