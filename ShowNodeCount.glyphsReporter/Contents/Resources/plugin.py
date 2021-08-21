59efafc8773d1a32f8c7f34e98be0936f91a4da5
Rainer Scheichelbauer <res@glyphsapp.com>
2021-08-21 15:35:40 +0100

    Fix crash, complete py3 and SDK update

--- ShowNodeCount.glyphsReporter/Contents/Resources/plugin.py
+++ ShowNodeCount.glyphsReporter/Contents/Resources/plugin.py
@@ -20,8 +20,6 @@ from GlyphsApp.plugins import *
 # import sys, os, re
 import math
 
-GlyphsReporterProtocol = objc.protocolNamed( "GlyphsReporter" )
-
 class ShowNodeCount(ReporterPlugin):
 
 	@objc.python_method
@@ -31,32 +29,19 @@ class ShowNodeCount(ReporterPlugin):
 			})
 
 	@objc.python_method
-	def drawTextAtPoint( self, text, textPosition, fontSize=12.0, fontColor=NSColor.colorWithCalibratedRed_green_blue_alpha_( 0.4, 0, .6, 1 ) ):
-		glyphEditView = self.controller.graphicView()
-		currentZoom = self.getScale()
-		fontAttributes = { 
-			NSFontAttributeName: NSFont.labelFontOfSize_( fontSize/currentZoom ),
-			NSForegroundColorAttributeName: fontColor }
-		displayText = NSAttributedString.alloc().initWithString_attributes_( text, fontAttributes )
-		textAlignment = 2 # top left: 6, top center: 7, top right: 8, center left: 3, center center: 4, center right: 5, bottom left: 0, bottom center: 1, bottom right: 2
-		displayText.drawAtPoint_( textPosition )
-
-	@objc.python_method
 	def drawNodeCount( self, Layer ):
-		Glyph = Layer.parent
-		Font = Glyph.parent
-		selectedLayer = Font.selectedLayers[0]
+		FontMaster = Layer.associatedFontMaster()
+		xHeight = FontMaster.xHeight
+		angle = FontMaster.italicAngle
 
-		xHeight = Font.selectedFontMaster.xHeight
-		angle = Font.selectedFontMaster.italicAngle
-		# rotation point is half of x-height
+		# rotation point is half of x-height:
 		offset = math.tan(math.radians(angle)) * xHeight/2
 
-		nodeCounter = 0
-		for thisPath in selectedLayer.paths:
-			nodeCounter += len(thisPath.nodes)
+		nodeCount = 0
+		for thisPath in Layer.paths:
+			nodeCount += len(thisPath.nodes)
 
-		self.drawTextAtPoint( u"· %s" % nodeCounter, (-25 - offset, 5) )
+		self.drawTextAtPoint( "· %s" % nodeCount, NSPoint(-25-offset, 5) )
 
 	@objc.python_method
 	def background(self, layer):
@@ -85,17 +70,4 @@ class ShowNodeCount(ReporterPlugin):
 		"""Please leave this method unchanged"""
 		return __file__
 
-	@objc.python_method
-	def getScale( self ):
-		try:
-			return self.controller.graphicView().scale()
-		except:
-			return 1.0
-
-	@objc.python_method
-	def setController_( self, Controller ):
-		try:
-			self.controller = Controller
-		except Exception as e:
-			pass
 
